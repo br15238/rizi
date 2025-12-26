e1c11f2bfd9b933b073cb70152e0318189418b8a
suanriz <suanriz@gmail.com>
2025-12-26 23:42:39 +0800

    init

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+
+const IndexMenu = defineAsyncComponent(() =>
+  import('@/components/index/menu.vue')
+)
+const IndexStore = defineAsyncComponent(() =>
+  import('@/components/index/store.vue')
+)
+const IndexConcept = defineAsyncComponent(() =>
+  import('@/components/index/concept.vue')
+)
+const IndexInfo = defineAsyncComponent(() =>
+  import('@/components/index/info.vue')
+)
+</script>
+
 <template>
   <IndexCarousel />
   <div class="contentWrap mb-0">
