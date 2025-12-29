36a33e6dab28ea1e25f834a665f4480bfae4763d
br15238 <br15238@925925.com>
2025-12-29 09:58:56 +0800

    init

--- app/components/Divider.vue
+++ app/components/Divider.vue
@@ -9,7 +9,8 @@ const { app: { baseURL } } = useRuntimeConfig()
 
 <template>
   <div
-    :class="['aspect-[924/48] max-w-[960px] w-full min-h-[40px] bg-center bg-no-repeat bg-cover flex items-center justify-center m-[30px_auto_20px] md:m-[30px_auto_4%]', `bg-[url(${baseURL}img/divider.webp)]`]"
+    class="aspect-[924/48] max-w-[960px] w-full min-h-[40px] bg-center bg-no-repeat bg-cover flex items-center justify-center m-[30px_auto_20px] md:m-[30px_auto_4%]"
+    :style="{ backgroundImage: `url(${baseURL}img/divider.webp)` }"
   >
     <h2 class="text-center text-[20px] leading-[20px] font-[500] tracking-[.04em] text-[var(--mainRed)]">
       {{ prop.title }}
