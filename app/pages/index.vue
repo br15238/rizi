923a99984c1f87b03c82a53925a8c17693dac8bd
br15238 <br15238@925925.com>
2025-12-29 13:02:08 +0800

    init

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,5 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-
-const showSlider = ref(false)
-
-onMounted(() => {
-  const idle =
-    window.requestIdleCallback ??
-    ((cb: () => void) => setTimeout(cb, 2000))
-
-  idle(() => {
-    showSlider.value = true
-  })
-})
-</script>
-
 <template>
-  <div class="relative aspect-[1280/533]">
-    <IndexHeroStatic v-if="!showSlider" class="absolute inset-0" />
-    <ClientOnly>
-      <LazyIndexCarousel v-if="showSlider" class="absolute inset-0" />
-    </ClientOnly>
-  </div>
+  <IndexCarousel />
   <div class="contentWrap mb-0">
     <IndexNews />
     <LazyIndexMenu />
